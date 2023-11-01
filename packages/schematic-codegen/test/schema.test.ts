d7a6849f078c0d5be944690498e2bacecf7001ca
Jason Prasad <jasongprasad@gmail.com>
2023-11-01 07:54:51 -0400

    Starting to build grammer

--- packages/schematic-codegen/test/schema.test.ts
+++ packages/schematic-codegen/test/schema.test.ts
@@ -1,3 +1,8 @@
+import grammar from '../src/schema.ohm-bundle';
+
 describe("schema", () => {
-  it("should", () => {});
+  it("should match 42", () => {
+    const matchResult = grammar.match("42");
+    expect(matchResult.succeeded()).toBe(true);
+  });
 });
