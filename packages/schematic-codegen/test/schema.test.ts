28f73adc4b2a87c65f50974a1007e13f8d150c50
Jason Prasad <jasongprasad@gmail.com>
2023-11-02 16:24:18 -0400

    Add to schema

--- packages/schematic-codegen/test/schema.test.ts
+++ packages/schematic-codegen/test/schema.test.ts
@@ -1,10 +1,14 @@
 import grammar from "../src/schema.ohm-bundle";
 
-var input = `component Text { verbatim: string; }`;
-
 describe("grammar", () => {
   it("should match text component", () => {
+    // Given
+    const input = `component Text { verbatim: string; }`;
+
+    // When
     const matchResult = grammar.match(input);
+
+    // Then
     expect(matchResult.succeeded()).toBe(true);
   });
 });
